19f707506c4482b406fb8a634663c89c5a3fc916
cataclysm1987 <conklin.eric@gmail.com>
2017-03-06 15:10:45 -0500

    add project files

---
+++ DebugEight1/DebugEight1/Program.cs
@@ -0,0 +1,38 @@
+﻿// GetData() method accepts order number and quantity
+// that are used in the Main() method
+// Price is $3.99 each
+using System;
+using System.Security.Cryptography;
+using static System.Console;
+class DebugEight1
+{
+    static void Main()
+    {
+        int orderNum, quantity;
+        const double PRICE_EACH = 3.99;
+        GetData(out orderNum, out quantity);
+        double total = quantity * PRICE_EACH;
+        WriteLine("Order #{0}. Quantity ordered = {1}",
+            orderNum, quantity);
+        WriteLine("Total is {0}", total.ToString("C"));
+    }
+    private static void GetData(out int order, out int amount)
+    {
+        try
+        {
+            Write("Enter order number ");
+            order = Convert.ToInt32(ReadLine());
+            Write("Enter quantity ");
+            amount = Convert.ToInt32(ReadLine());
+        }
+        catch (SystemException)
+        {
+            order = 0;
+            amount = 0;
+            Console.WriteLine("Error. Invalid value. Exiting program.");
+            Environment.Exit(0);
+        }
+       
+    }
+}
+
---
+++ DebugEight3/DebugEight3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Program demonstrates method that can be called
+// with one, two, or three arguments
+// Tuition is $80 per credit
+// Default district code is I.
+// If student is in-district (code I), then there is no
+// out-of-district fee, which is $300
+// Default scholarship status is false.
+// If student is on scholarship, tuition is free
+using System;
+using static System.Console;
+class DebugEight3
+{
+    static void Main()
+    {
+        WriteLine("Tuition is {0}", CalculateTuition(15));
+        WriteLine("Tuition is {0}", CalculateTuition(15, 'O'));
+        WriteLine("Tuition is {0}", CalculateTuition(15, 'O', true));
+    }
+
+    private static double CalculateTuition(double credits, char code)
+    {
+        const double RATE = 80.00;
+        const double OUT_DISTRICT_FEE = 300.00;
+        var tuition = credits * RATE;
+        if (code != 'I')
+            tuition += OUT_DISTRICT_FEE;
+        return tuition;
+    }
+
+    private static double CalculateTuition(double credits)
+    {
+        const double RATE = 80.00;
+        var tuition = credits * RATE;
+        return tuition;
+    }
+
+    public static double CalculateTuition(double credits, char code, bool scholarship)
+    {
+        const double RATE = 80.00;
+        const double OUT_DISTRICT_FEE = 300.00;
+        var tuition = credits * RATE;
+        if (code != 'I')
+            tuition += OUT_DISTRICT_FEE;
+        if (scholarship)
+            tuition = 0;
+        return tuition;
+    }
+}
